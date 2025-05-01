aaa5ee889334e4b399949b24a5e1b40013ffee3e
APersonWithNoName <wyb20102024@outlook.com>
2025-05-01 17:46:09 +0800

    fix: build deps

--- dep.sh
+++ dep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "deb-src https://deb.debian.org/debian/ bookworm main contrib non-free" >> /etc/apt/sources.list
+#echo "deb-src https://deb.debian.org/debian/ bookworm main contrib non-free" >> /etc/apt/sources.list
 
 apt update -y && apt upgrade -y
-apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev build-essential libelf-dev libncurses5-dev dwarve
-apt build-dep -y linux
+apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev build-essential libelf-dev libncurses5-dev dwarves
+#apt build-dep -y linux
